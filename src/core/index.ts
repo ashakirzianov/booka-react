3951f384b944d29cd5e9951577f5343701485930
ashakirzianov <ashakirzianov@icloud.com>
2019-10-26 16:57:51 +0700

    Implement 'ConnectedProvider'

---
+++ src/core/index.ts
@@ -0,0 +1,1 @@
+export { ConnectedProvider } from './store';
--- src/core/store.ts
+++ src/core/store.ts
@@ -1,5 +1,13 @@
+import * as React from 'react';
 import { createStore } from 'redux';
 
+import { Provider } from 'react-redux';
+
 import { rootReducer } from './reducers';
+import { AppAction } from '../model';
 
 const store = createStore(rootReducer);
+
+class AppProvider extends Provider<AppAction> { }
+export const ConnectedProvider: React.SFC = ({ children }) =>
+    React.createElement(AppProvider, { store }, children);
