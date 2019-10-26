72ed23930c29eab5a5c29e5949452620a6225043
ashakirzianov <ashakirzianov@icloud.com>
2019-10-26 16:41:45 +0700

    Implement primitive reducer

---
+++ src/core/reducers.ts
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import { AppState, AppAction } from "../model";
+
+function books(state: AppState['books'] = [], action: AppAction) {
+    switch (action.type) {
+        case 'ALLBOOKS_FULFILLED':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+export const reducer = combineReducers<AppState, AppAction>({
+    books,
+});
