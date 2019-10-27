91feb4888f8d912c73a93eddf4f162deba4010a5
ashakirzianov <ashakirzianov@icloud.com>
2019-10-27 13:59:09 +0700

    Configure 'redux-observable' middleware

--- src/core/store.ts
+++ src/core/store.ts
@@ -1,15 +1,34 @@
+/*global globalThis*/
 import React from 'react';
-import { createStore } from 'redux';
-
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import { rootReducer } from './reducers';
+import { rootEpic } from './epics';
+import { createEpicMiddleware } from 'redux-observable';
 
-const store = createStore(rootReducer, {
-    books: [{
-        title: 'Hello world',
-    }],
-});
+function configureStore() {
+    const epicMiddleware = createEpicMiddleware();
+    const composeEnhancers: typeof compose = (globalThis.window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const s = createStore(
+        rootReducer,
+        {
+            books: [{
+                title: 'Hello world',
+            }],
+        },
+        composeEnhancers(
+            applyMiddleware(
+                epicMiddleware,
+            ),
+        ),
+    );
+    epicMiddleware.run(rootEpic);
+
+    return s;
+}
+
+const store = configureStore();
 
 export const ConnectedProvider: React.SFC = ({ children }) =>
     React.createElement(Provider, { store }, children);
