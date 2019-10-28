968a3f410bc53a7997327ba6c3410034b9659e82
ashakirzianov <ashakirzianov@icloud.com>
2019-10-28 14:24:59 +0700

    Add debug/prod configs

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,18 @@
-export function config() {
+function prodConfig() {
     return {
         libUrl: 'https://booka-lib.herokuapp.com',
     };
 }
+
+function debugConfig(): AppConfig {
+    return {
+        ...prodConfig(),
+    };
+}
+
+export function config() {
+    return process.env.NODE_ENV === 'development'
+        ? debugConfig()
+        : prodConfig();
+}
+export type AppConfig = ReturnType<typeof prodConfig>;
