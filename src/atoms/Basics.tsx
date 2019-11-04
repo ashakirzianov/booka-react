8864d9600b24b47f1599488dc85941e1e88c9198
ashakirzianov <ashakirzianov@icloud.com>
2019-11-04 18:29:28 +0700

    Remove //TODO comment

--- src/atoms/Basics.tsx
+++ src/atoms/Basics.tsx
@@ -5,7 +5,6 @@ import { ActivityIndicator, View } from 'react-native';
 import { Theme, PaletteColor, colors, getFontSize } from './theme';
 import { percent, point, Size, WithChildren, defaults } from './common';
 
-// TODO: remove ?
 export type TextProps = {
     fontFamily?: keyof Theme['fontFamilies'],
     fontSize?: keyof Theme['fontSizes'],
