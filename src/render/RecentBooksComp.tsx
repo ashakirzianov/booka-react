b0d3f48db6fafd99b152a0fee44b0163833f69d5
ashakirzianov <ashakirzianov@icloud.com>
2020-02-11 15:03:42 -0300

    RecentBooksComp

---
+++ src/render/RecentBooksComp.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { pathLessThan } from 'booka-common';
+import {
+    RecentBooksState, RecentBook, RecentBookLocation,
+} from '../ducks';
+import { useTheme, useAppSelector } from '../core';
+import {
+    Column, Themed,
+} from '../atoms';
+
+export function RecentBooksConnected() {
+    const state = useAppSelector(s => s.recentBooks);
+
+    const theme = useTheme();
+
+    return <RecentBooksComp
+        theme={theme}
+        state={state}
+    />;
+}
+
+type RecentBooksProps = Themed & {
+    state: RecentBooksState,
+};
+function RecentBooksComp({ state }: RecentBooksProps) {
+    return <Column>
+        {
+            state.map((recentBook, idx) => {
+                return <RecentBookComp
+                    key={idx}
+                    recentBook={recentBook}
+                />;
+            })
+        }
+    </Column>;
+}
+
+function RecentBookComp({ recentBook }: { recentBook: RecentBook }) {
+    if (recentBook.locations.length === 0) {
+        return null;
+    }
+    const mostRecent = getMostRecentLocation(recentBook.locations);
+    const farthest = getFarthestLocation(recentBook.locations);
+    // TODO: use proper book links
+    if (mostRecent === farthest) {
+        return <span>Id: {recentBook.id}, path: {mostRecent}</span>;
+    } else {
+        return <span>Id: {recentBook.id}, recent: {mostRecent}, farthest: {farthest}</span>;
+    }
+}
+
+function getMostRecentLocation(rbls: RecentBookLocation[]): RecentBookLocation {
+    return rbls.reduce(
+        (last, curr) =>
+            curr.created > last.created ? curr : last
+    );
+}
+
+function getFarthestLocation(rbls: RecentBookLocation[]): RecentBookLocation {
+    return rbls.reduce(
+        (farthest, curr) =>
+            pathLessThan(farthest.path, curr.path) ? curr : farthest
+    );
+}
