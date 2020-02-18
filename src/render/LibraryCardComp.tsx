f80f5ba3dc18edd67da428d316325f5f6bf8471c
ashakirzianov <ashakirzianov@icloud.com>
2020-02-18 17:55:25 -0300

    Read button

--- src/render/LibraryCardComp.tsx
+++ src/render/LibraryCardComp.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import { LibraryCard, Callback } from 'booka-common';
-import { BookCoverComp, Column, Modal, Themed } from '../atoms';
-import { useTheme, useAppDispatch, useAppSelector } from '../core';
+import { BookCoverComp, Column, Modal, Themed, navigate } from '../atoms';
+import { useTheme, useAppDispatch, useAppSelector, linkToString } from '../core';
 
-export type LibraryCardProps = Themed & {
-    card: LibraryCard,
-    toggleCard: Callback,
-};
 export function LibraryCardConnected() {
     const dispatch = useAppDispatch();
     const card = useAppSelector(s => s.library.show);
@@ -15,6 +11,11 @@ export function LibraryCardConnected() {
         type: 'card-close',
     }), [dispatch]);
 
+    const readFromStart = React.useCallback((bookId: string) => navigate(linkToString({
+        link: 'book',
+        bookId,
+    })), []);
+
     const theme = useTheme();
 
     if (card) {
@@ -22,24 +23,28 @@ export function LibraryCardConnected() {
             theme={theme}
             card={card}
             toggleCard={closeCard}
+            readFromStart={readFromStart}
         />;
     } else {
         return null;
     }
 }
 
-function LibraryCardModal({ theme, toggleCard, card }: LibraryCardProps) {
+type LibraryCardProps = Themed & {
+    card: LibraryCard,
+    toggleCard: Callback,
+    readFromStart: Callback<string>,
+};
+function LibraryCardModal({ theme, toggleCard, card, readFromStart }: LibraryCardProps) {
     return <Modal
         theme={theme}
         toggle={toggleCard}
         open={true}
     >
-        <LibraryCardView {...card} />
+        <Column>
+            <BookCoverComp {...card} />
+            <span>{card.title}</span>
+            <span onClick={() => readFromStart(card.id)}>Read</span>
+        </Column>
     </Modal>;
-}
-function LibraryCardView(props: LibraryCard) {
-    return <Column>
-        <BookCoverComp {...props} />
-        <span>{props.title}</span>
-    </Column>;
 }
