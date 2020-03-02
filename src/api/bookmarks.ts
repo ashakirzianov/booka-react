4ddb5c3b1c7383726f82e32db851fb46ff7e3aa3
ashakirzianov <ashakirzianov@icloud.com>
2020-03-02 15:43:43 -0300

    Extract current positions api to it's own file

--- src/api/bookmarks.ts
+++ src/api/bookmarks.ts
@@ -1,33 +1,9 @@
-import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { BackContract, AuthToken, BookPath, EntitySource, BookmarkPost, ResolvedCurrentPosition } from 'booka-common';
+import { BackContract, AuthToken, BookmarkPost } from 'booka-common';
 import { createFetcher } from './fetcher';
 import { config } from '../config';
 
 const back = createFetcher<BackContract>(config().backUrl);
-
-export function getCurrentPositions(token: AuthToken): Observable<ResolvedCurrentPosition[]> {
-    return back.get('/current-position', {
-        auth: token.token,
-    }).pipe(
-        map(res => res.value),
-    );
-}
-
-export function sendCurrentPathUpdate({ bookId, path, source, token }: {
-    bookId: string,
-    path: BookPath,
-    source: EntitySource,
-    token: AuthToken,
-}) {
-    const created = new Date(Date.now());
-    return back.put('/current-position', {
-        auth: token.token,
-        body: {
-            source, bookId, path, created,
-        },
-    });
-}
 
 export function getBookmarks(bookId: string, token: AuthToken) {
     return back.get('/bookmarks', {
