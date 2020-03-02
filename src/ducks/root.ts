37d748ab4f6c49116a220179bbb9c56865c2f7f3
ashakirzianov <ashakirzianov@icloud.com>
2020-03-02 14:35:05 -0300

    Sync state/actions/reducer

--- src/ducks/root.ts
+++ src/ducks/root.ts
@@ -12,7 +12,7 @@ import { collectionsReducer } from './collections';
 import { bookmarksReducer } from './bookmarks';
 import { highlightsReducer } from './highlights';
 import { changesReducer } from './changes';
-import { syncEpic } from './sync';
+import { syncEpic, syncReducer } from './sync';
 
 export const rootReducer = combineReducers<AppState, AppAction>({
     library: libraryReducer,
@@ -26,6 +26,7 @@ export const rootReducer = combineReducers<AppState, AppAction>({
     bookmarks: bookmarksReducer,
     highlights: highlightsReducer,
     changes: changesReducer,
+    sync: syncReducer,
 });
 
 export const rootEpic = combineEpics(
