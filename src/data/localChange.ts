f82e3d9f24ff48da7f88c316c2a36c1dc1d50aeb
ashakirzianov <ashakirzianov@icloud.com>
2020-03-06 15:32:23 -0300

    Define local changes

---
+++ src/data/localChange.ts
@@ -0,0 +1,53 @@
+import { BookPath, HighlightGroup, BookRange } from 'booka-common';
+
+type DefChange<Key extends string> = {
+    change: Key,
+};
+
+type AddBookmarkChange = DefChange<'bookmark-add'> & {
+    bookId: string,
+    path: BookPath,
+};
+
+type RemoveBookmarkChange = DefChange<'bookmark-remove'> & {
+    bookmarkId: string,
+};
+
+type AddHighlightChange = DefChange<'highlight-add'> & {
+    bookId: string,
+    group: HighlightGroup,
+    range: BookRange,
+};
+
+type RemoveHighlightChange = DefChange<'highlight-remove'> & {
+    highlightId: string,
+};
+
+type UpdateHighlightChange = DefChange<'highlight-update'> & {
+    highlightId: string,
+    group?: HighlightGroup,
+};
+
+type UpdateCurrentPositionChange = DefChange<'current-position-update'> & {
+    bookId: string,
+    path: BookPath,
+};
+
+export type LocalChange =
+    | AddBookmarkChange | RemoveBookmarkChange
+    | AddHighlightChange | RemoveHighlightChange | UpdateHighlightChange
+    | UpdateCurrentPositionChange
+    ;
+
+let local: LocalChange[] = [];
+export function addLocalChange(change: LocalChange) {
+    local = [...local, change];
+}
+
+export function removeLocalChange(change: LocalChange) {
+    local = local.filter(c => c !== change);
+}
+
+export function applyLocalChanges<T>(state: T, reducer: (s: T, ch: LocalChange) => T): T {
+    return local.reduce(reducer, state);
+}
