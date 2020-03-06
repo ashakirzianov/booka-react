a42db04a00aefa6929e5b5e484ddff7a0b9883ab
ashakirzianov <ashakirzianov@icloud.com>
2020-03-06 19:28:29 -0300

    Positions data hook

--- src/application/dataHooks.ts
+++ src/application/dataHooks.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from 'react';
 import {
-    AuthToken, Bookmark, Highlight,
+    AuthToken, Bookmark, Highlight, ResolvedCurrentPosition,
 } from 'booka-common';
 import { dataProvider } from '../data';
 
@@ -15,7 +15,7 @@ export function useBookmarksData(bookId: string, token?: AuthToken) {
     const [bookmarks, setBookmarks] = useState<BookmarksState>([]);
     const { subject, add, remove } = useMemo(
         () => data.bookmarksForId(bookId, token),
-        [bookId, token],
+        [bookId, token, data],
     );
     useEffect(() => {
         const sub = subject.subscribe(setBookmarks);
@@ -30,11 +30,26 @@ export function useHighlightsData(bookId: string, token?: AuthToken) {
     const [highlights, setHighlights] = useState<HighlightsState>([]);
     const { subject, add, remove } = useMemo(
         () => data.highlightsForId(bookId, token),
-        [bookId, token],
+        [bookId, token, data],
     );
     useEffect(() => {
         const sub = subject.subscribe(setHighlights);
         return () => sub.unsubscribe();
     }, [subject]);
     return { highlights, add, remove };
+}
+
+type PositionsState = ResolvedCurrentPosition[];
+export function usePositionsData(token?: AuthToken) {
+    const data = useDataProvider();
+    const [positions, setPositions] = useState<PositionsState>([]);
+    const { subject, add } = useMemo(
+        () => data.currentPositions(token),
+        [token, data],
+    );
+    useEffect(() => {
+        const sub = subject.subscribe(setPositions);
+        return () => sub.unsubscribe();
+    }, [subject]);
+    return { positions, add };
 }
