34e9ca5c599615ebb7b2881f0d1a02a75ec87189
ashakirzianov <ashakirzianov@icloud.com>
2020-03-06 19:25:27 -0300

    Highlights data hook

--- src/application/dataHooks.ts
+++ src/application/dataHooks.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect, useMemo } from 'react';
 import {
-    AuthToken, Bookmark,
+    AuthToken, Bookmark, Highlight,
 } from 'booka-common';
 import { dataProvider } from '../data';
 
@@ -13,10 +13,28 @@ type BookmarksState = Bookmark[];
 export function useBookmarksData(bookId: string, token?: AuthToken) {
     const data = useDataProvider();
     const [bookmarks, setBookmarks] = useState<BookmarksState>([]);
-    const { subject, add, remove } = useMemo(() => data.bookmarksForId(bookId, token), [bookId, token]);
+    const { subject, add, remove } = useMemo(
+        () => data.bookmarksForId(bookId, token),
+        [bookId, token],
+    );
     useEffect(() => {
         const sub = subject.subscribe(setBookmarks);
         return () => sub.unsubscribe();
     }, [subject]);
     return { bookmarks, add, remove };
+}
+
+type HighlightsState = Highlight[];
+export function useHighlightsData(bookId: string, token?: AuthToken) {
+    const data = useDataProvider();
+    const [highlights, setHighlights] = useState<HighlightsState>([]);
+    const { subject, add, remove } = useMemo(
+        () => data.highlightsForId(bookId, token),
+        [bookId, token],
+    );
+    useEffect(() => {
+        const sub = subject.subscribe(setHighlights);
+        return () => sub.unsubscribe();
+    }, [subject]);
+    return { highlights, add, remove };
 }
