5be1fb9ee999d16c6aebc90b2942817d011c29f8
ashakirzianov <ashakirzianov@icloud.com>
2020-03-16 20:16:27 -0300

    Move 'updateSearch' to 'utils'

--- src/application/index.ts
+++ src/application/index.ts
@@ -2,3 +2,4 @@ export * from './dataHooks';
 export { ConnectedProvider } from './store';
 export * from './urlHooks';
 export * from './utilityHooks';
+export * from './utils';
--- src/application/urlHooks.ts
+++ src/application/urlHooks.ts
@@ -1,11 +1,11 @@
 import { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
 import { History } from 'history';
-import { parse, stringify } from 'query-string';
 
 import {
     BookPath, pathToString, BookRange, rangeToString,
 } from 'booka-common';
+import { updateSearch } from './utils';
 
 export function useUrlActions() {
     const history = useHistory();
@@ -34,12 +34,4 @@ function replaceHistorySearch(history: History, key: string, value: string | und
         ...history.location,
         search: updateSearch(history.location.search, key, value),
     });
-}
-
-// TODO: move to utils ?
-export function updateSearch(search: string, key: string, value: string | undefined | null) {
-    const obj = parse(search);
-    obj[key] = value;
-    const result = stringify(obj);
-    return result ? `?${result}` : '';
 }
