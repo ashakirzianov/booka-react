2aa12d782c0267863bce2af973d4b1843090cf04
ashakirzianov <ashakirzianov@icloud.com>
2020-03-20 18:58:16 -0300

    Move 'BookList' to '/render'

--- src/render/BookList.tsx
+++ src/render/BookList.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { LibraryCard } from 'booka-common';
 import { Themed } from '../application';
-import { BookTile } from './BookTile';
-import { GridList } from './GridList';
+import { BookTile, GridList } from '../controls';
 
 export function BookList({ books, theme }: Themed & {
     books: LibraryCard[],
