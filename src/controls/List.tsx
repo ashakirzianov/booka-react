811c52ba98c55a9b604e9657130d900fce3f5b81
ashakirzianov <ashakirzianov@icloud.com>
2020-03-25 13:13:32 -0300

    MenuList

--- src/controls/List.tsx
+++ src/controls/List.tsx
@@ -1,6 +1,10 @@
+// eslint-disable-next-line
 import React from 'react';
+/** @jsx jsx */
+import { jsx } from '@emotion/core';
+
 import { HasChildren } from './common';
-import { Themed } from './theme';
+import { Themed, colors, getFontSize } from './theme';
 
 export function GridList({ theme, children }: Themed & HasChildren) {
     return <div style={{
@@ -12,5 +16,39 @@ export function GridList({ theme, children }: Themed & HasChildren) {
         justifyContent: 'flex-start',
     }}>
         {children}
+    </div>;
+}
+
+export function MenuList({ theme, children }: Themed & HasChildren) {
+    return <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+    }}>
+        {children}
+    </div>;
+}
+
+export function MenuListItem({ theme, left, right }: Themed & {
+    left?: string,
+    right?: string,
+}) {
+    return <div css={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        color: colors(theme).text,
+        fontSize: getFontSize(theme, 'small'),
+        fontFamily: theme.fontFamilies.menu,
+        '&:hover': {
+            color: colors(theme).primary,
+            background: colors(theme).highlight,
+        },
+    }}>
+        <span>
+            {left}
+        </span>
+        <span>
+            {right}
+        </span>
     </div>;
 }
