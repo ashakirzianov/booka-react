eb0e84c735deac58c846eefac304f5f84c36585d
ashakirzianov <ashakirzianov@icloud.com>
2020-03-30 13:56:35 -0300

    UploadPanel

--- src/render/UploadButton.tsx
+++ src/render/UploadButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useTheme } from '../application';
-import { IconButton, WithPopover, Label } from '../controls';
+import { IconButton, WithPopover, Label, View, ActionButton, normalPadding } from '../controls';
 import { Themed } from '../core';
 
 export function UploadButton() {
@@ -21,8 +21,18 @@ export function UploadButton() {
 }
 
 export function UploadPanel({ theme }: Themed) {
-    return <Label
-        theme={theme}
-        text='Upload'
-    />;
+    return <View style={{
+        alignItems: 'center',
+        padding: normalPadding,
+    }}>
+        <Label
+            theme={theme}
+            text={`Select '.epub' file to upload`}
+        />
+        <ActionButton
+            theme={theme}
+            color='positive'
+            text='Select'
+        />
+    </View>;
 }
