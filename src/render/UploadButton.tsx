7bb76b8250d8b13cd2669a30bbd0db5db0344430
ashakirzianov <ashakirzianov@icloud.com>
2020-03-30 14:09:36 -0300

    Show select file dialog

--- src/render/UploadButton.tsx
+++ src/render/UploadButton.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useTheme } from '../application';
-import { IconButton, WithPopover, Label, View, ActionButton, normalPadding } from '../controls';
+import {
+    IconButton, WithPopover, Label, View, ActionButton,
+    normalPadding, SelectFileDialog, SelectFileDialogRef,
+} from '../controls';
 import { Themed } from '../core';
 
 export function UploadButton() {
@@ -21,10 +24,19 @@ export function UploadButton() {
 }
 
 export function UploadPanel({ theme }: Themed) {
+    const dialogRef = useRef<SelectFileDialogRef>();
     return <View style={{
         alignItems: 'center',
         padding: normalPadding,
     }}>
+        <SelectFileDialog
+            accept='application/epub+zip'
+            dataKey='book'
+            refCallback={r => dialogRef.current = r}
+            onFilesSelected={async data => {
+                return undefined;
+            }}
+        />
         <Label
             theme={theme}
             text={`Select '.epub' file to upload`}
@@ -33,6 +45,11 @@ export function UploadPanel({ theme }: Themed) {
             theme={theme}
             color='positive'
             text='Select'
+            callback={() => {
+                if (dialogRef.current) {
+                    dialogRef.current.show();
+                }
+            }}
         />
     </View>;
 }
