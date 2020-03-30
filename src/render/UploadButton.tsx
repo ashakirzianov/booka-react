7a5768d4358f1cbed793d57aaa1a5e6f445c4378
ashakirzianov <ashakirzianov@icloud.com>
2020-03-30 13:52:38 -0300

    Upload popover

--- src/render/UploadButton.tsx
+++ src/render/UploadButton.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { useTheme } from '../application';
-import { IconButton } from '../controls';
+import { IconButton, WithPopover, Label } from '../controls';
+import { Themed } from '../core';
 
 export function UploadButton() {
     const { theme } = useTheme();
 
-    return <IconButton
+    return <WithPopover
         theme={theme}
-        icon='upload'
+        popoverPlacement='bottom'
+        body={<UploadPanel
+            theme={theme}
+        />}
+    >
+        <IconButton
+            theme={theme}
+            icon='upload'
+        />
+    </WithPopover>;
+}
+
+export function UploadPanel({ theme }: Themed) {
+    return <Label
+        theme={theme}
+        text='Upload'
     />;
 }
