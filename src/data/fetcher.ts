0a01c83c33d025d13447f6953c559733cad723c2
ashakirzianov <ashakirzianov@icloud.com>
2020-03-30 23:17:30 -0300

    Fix fetcher

--- src/data/fetcher.ts
+++ src/data/fetcher.ts
@@ -27,13 +27,11 @@ export function createFetcher<C extends ApiContract>(baseUrl: string): Fetcher<C
         return (path, param) => {
             const url = baseUrl + replaceParams(path, param.params) + queryToString(param.query);
             const formData = param.extra && param.extra.postData;
-            // TODO: test form data works
             const body = formData || param.body;
             const req: AjaxRequest = {
                 url, method, body,
                 responseType: 'json',
                 headers: {
-                    'Content-Type': 'application/json',
                     ...param.extra && param.extra.headers,
                     ...param.auth && { Authorization: `Bearer ${param.auth}` },
                 },
