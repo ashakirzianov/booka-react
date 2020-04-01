b06c007f87c278446e8e5708bcd54c3f9ddf6680
ashakirzianov <ashakirzianov@icloud.com>
2020-03-31 23:23:38 -0300

    Disable multiline 'BookList'

--- src/render/BookList.tsx
+++ src/render/BookList.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { LibraryCard } from 'booka-common';
 import { Themed } from '../core';
-import { GridList, bookCoverHeight } from '../controls';
+import { GridList } from '../controls';
 import { LibraryCardLink } from './LibraryCardTile';
 
-export function BookList({ books, lines, theme }: Themed & {
+export function BookList({ books, theme }: Themed & {
     books: LibraryCard[],
     lines?: number,
 }) {
     return <GridList
         theme={theme}
-        // TODO: Rethink this truly terrible solution
-        maxHeight={
-            lines
-                ? (lines + 1) * bookCoverHeight
-                : undefined
-        }
     >
         {
             books.map((card, idx) =>
