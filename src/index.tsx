065c7e6aff4ed9f7dc6b962b0b5c937170feba3b
ashakirzianov <ashakirzianov@icloud.com>
2020-04-05 15:48:02 -0300

    Do not register service workers

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import { initWhyDidYouRender } from './utils';
 
 (async function init() {
@@ -12,5 +12,5 @@ import { initWhyDidYouRender } from './utils';
     // If you want your app to work offline and load faster, you can change
     // unregister() to register() below. Note this comes with some pitfalls.
     // Learn more about service workers: https://bit.ly/CRA-PWA
-    serviceWorker.register();
+    // serviceWorker.register();
 })();
