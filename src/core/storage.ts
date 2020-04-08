118b7db5756cc829bb99be75716e569820e75ee7
ashakirzianov <ashakirzianov@icloud.com>
2020-04-08 13:21:49 -0300

    In memory cells

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -8,37 +8,40 @@ export type StorageCell<T> = {
 };
 export type AppStorage = ReturnType<typeof createStorage>;
 export function createStorage(prefix: string) {
-    type CellData<T> = {
-        value: T,
-        date: number,
-    };
-    function restoreCellData<T>(key: string): CellData<T> | undefined {
-        return storeApi.get(key);
-    }
     function makeCell<T>(key: string): StorageCell<T> {
         const fullKey = `${prefix}:${key}`;
+        let inMemory: CellData | undefined;
+        type CellData = {
+            value: T,
+            date: number,
+        };
+        function restoreCellData(): CellData | undefined {
+            return inMemory ?? storeApi.get(fullKey);
+        }
         return {
             store(value) {
                 try {
-                    const data: CellData<T> = {
+                    const data: CellData = {
                         value, date: Date.now(),
                     };
                     storeApi.set(fullKey, data);
+                    inMemory = data;
                     return true;
                 } catch {
                     return false;
                 }
             },
             restore() {
-                const data = restoreCellData<T>(fullKey);
+                const data = restoreCellData();
                 return data?.value;
             },
             date() {
-                const date = restoreCellData(fullKey)?.date;
+                const date = restoreCellData()?.date;
                 return date ? new Date(date) : undefined;
             },
             clear() {
                 storeApi.remove(fullKey);
+                inMemory = undefined;
             },
         };
     }
