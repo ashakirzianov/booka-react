de961d74ea884ddd39b48c566b325ec93580e7bf
ashakirzianov <ashakirzianov@icloud.com>
2020-04-08 12:58:39 -0300

    Extend Storage type

--- src/core/storage.ts
+++ src/core/storage.ts
@@ -1,27 +1,58 @@
-import storeFn from 'store2';
+import storeApi from 'store2';
 
 export type StorageCell<T> = {
-    store: (data: T) => void,
+    store: (value: T) => boolean,
     restore: () => T | undefined,
+    date: () => Date | undefined,
+    clear: () => void,
 };
-
 export type Storage = ReturnType<typeof createStorage>;
-export function createStorage(prefix?: string) {
-    prefix = prefix ?? '<default>';
+export function createStorage(optPrefix?: string) {
+    const prefix = optPrefix ?? '<root>';
+    type CellData<T> = {
+        value: T,
+        date: number,
+    };
+    function restoreCellData<T>(key: string): CellData<T> | undefined {
+        return storeApi.get(key);
+    }
+    function makeCell<T>(key: string): StorageCell<T> {
+        const fullKey = `${prefix}:${key}`;
+        return {
+            store(value) {
+                try {
+                    const data: CellData<T> = {
+                        value, date: Date.now(),
+                    };
+                    storeApi.set(fullKey, data);
+                    return true;
+                } catch {
+                    return false;
+                }
+            },
+            restore() {
+                const data = restoreCellData<T>(fullKey);
+                return data?.value;
+            },
+            date() {
+                const date = restoreCellData(fullKey)?.date;
+                return date ? new Date(date) : undefined;
+            },
+            clear() {
+                storeApi.remove(fullKey);
+            },
+        };
+    }
+    function keys() {
+        return storeApi.keys().filter(key => key.startsWith(prefix));
+    }
     return {
-        cell<T>(key: string): StorageCell<T> {
-            const fullKey = `${prefix}:${key}`;
-            return {
-                store(data) {
-                    storeFn(fullKey, data);
-                },
-                restore() {
-                    return storeFn(fullKey);
-                },
-            };
+        cell: makeCell,
+        cells() {
+            return keys().map(makeCell);
         },
         sub(p: string) {
-            return createStorage(`${prefix}.${p}`);
+            return createStorage(`${prefix}/${p}`);
         },
     };
 }
