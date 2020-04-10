566b9b319023a26f7e9a3719c4f774f2aad2d286
ashakirzianov <ashakirzianov@icloud.com>
2020-04-10 10:59:23 -0300

    Refactor TagList

--- src/render/TagList.tsx
+++ src/render/TagList.tsx
@@ -1,42 +1,50 @@
 import React from 'react';
-import { Themed, PaletteColor } from '../core';
-import { LibraryCard, filterUndefined } from 'booka-common';
+import { Themed } from '../core';
+import { LibraryCard, KnownTag } from 'booka-common';
 import { TagLabel, View, regularSpace } from '../controls';
 
 export function TagList({ card, theme }: Themed & {
     card: LibraryCard,
 }) {
-    const data = getTagDescs(card);
     return <View style={{
         flexDirection: 'row',
         flexWrap: 'wrap',
         margin: regularSpace,
     }}>
         {
-            data.map(({ color, text }, idx) => {
-                return <TagLabel
+            card.tags.map((tag, idx) =>
+                <TabButton
                     key={idx}
                     theme={theme}
-                    color={color}
-                    text={text}
-                />;
-            })
+                    tag={tag}
+                />)
         }
     </View>;
 }
 
-type TagDesc = { color: PaletteColor, text: string };
-function getTagDescs(card: LibraryCard): TagDesc[] {
-    return filterUndefined(card.tags.map((tag): TagDesc | undefined => {
-        switch (tag.tag) {
-            case 'subject':
-                return { color: 'blue', text: tag.value };
-            case 'language':
-                return { color: 'green', text: `Language: ${tag.value.toUpperCase()}` };
-            case 'pg-index':
-                return { color: 'red', text: 'Project Gutenberg' };
-            default:
-                return undefined;
-        }
-    }));
+function TabButton({ tag, theme }: Themed & {
+    tag: KnownTag,
+}) {
+    switch (tag.tag) {
+        case 'subject':
+            return <TagLabel
+                theme={theme}
+                color='blue'
+                text={tag.value}
+            />;
+        case 'language':
+            return <TagLabel
+                theme={theme}
+                color='green'
+                text={`Language: ${tag.value.toUpperCase()}`}
+            />;
+        case 'pg-index':
+            return <TagLabel
+                theme={theme}
+                color='red'
+                text='Project Gutenberg'
+            />;
+        default:
+            return null;
+    }
 }
